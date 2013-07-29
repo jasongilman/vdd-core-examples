002e415602a9916ab203dd8ceaaba94207585067
Jason Gilman <jason@element84.com>
2013-07-28 20:00:40 -0400

    Moved visualization driver code into a driver.clj file.

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [vdd-core "0.1.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.3"]
-                                  [clj-http "0.7.4"]]}})
+                                  ; Added vdd-core as a development dependency
+                                  [vdd-core "0.1.0-SNAPSHOT"]]
+                   ; Add viz as a source path to load visualization drivers
+                   :source-paths ["viz"]}})
