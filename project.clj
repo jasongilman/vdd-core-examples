2e0887066d9de4065d2310db7f1c66403373b30b
Jason Gilman <jason@element84.com>
2013-09-01 20:56:00 -0400

    Updating project.clj to use vdd-core as a normal dependency.

--- project.clj
+++ project.clj
@@ -3,10 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [vdd-core "0.1.0"]]
   
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.3"]
-                                  ; Added vdd-core as a development dependency
-                                  [vdd-core "0.1.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.3"]]
                    ; Add viz as a source path to load visualization drivers
                    :source-paths ["viz"]}})
